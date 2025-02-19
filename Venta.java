f228da990da36295ada4b12ed03d6745fd57b969
Matt160503 <117934894+Matt160503@users.noreply.github.com>
2025-02-19 16:20:34 -0300

    Update Venta.java

--- Venta.java
+++ Venta.java
@@ -1,22 +1,58 @@
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.List;
 
 public class Venta {
     private int id;
     private List<Producto> productos;
     private double monto;
-    private String archivo_ventas;
+    private String archivo_ventas = "Ventas";
+    private List<Integer> cantidad;
 
-    public Venta(int id,List<Producto> productos){
+    //Constructor
+    public Venta(int id,List<Producto> productos,List<Integer> cantidad){
         this.id = id;
         this.productos = productos;
-        this.monto = monto;
-        this.archivo_ventas = archivo_ventas;
+        this.monto = calcularMonto();
+        this.cantidad = cantidad;
     }
 
+    //Funcion para generar el monto total de la venta
     public double calcularMonto(){
-        for (Producto p : productos){
-            Producto.getPrecio()
-            monto = monto ++ p;
+        double montoFinal = 0;
+        for (int i = 0; i<productos.size(); i++){
+            montoFinal += productos.get(i).getPrecio() * cantidad.get(i);
         }
+        return montoFinal;
+    }
+
+    //Creacion de documento para la persistencia de datos
+    public void registrarVentaCSV(){
+        try(FileWriter writer = new FileWriter(archivo_ventas, true)) {
+            StringBuilder linea = new StringBuilder();
+            linea.append(id).append(", ");
+            //Guardado de cada producto con su respectiva cantidad
+            for (int i = 0; i<productos.size(); i++){
+                linea.append(productos.get(i).getNombre()).append(": ").append(cantidad.get(i));
+            }
+            linea.append(", ").append(monto).append("\n");
+            writer.write(linea.toString());
+            System.out.println("Venta ingresada a: "+ archivo_ventas);
+        }
+
+        //Codigo en caso de que haya algun error al registrar la venta del producto
+        catch (IOException error){
+            System.err.println("No se pudo registrar la venta");
+        }
+    }
+
+    //Getters
+    public double getMonto() {
+        return monto;
+    }
+
+    //Setters
+    public void setMonto(double monto) {
+        this.monto = monto;
     }
 }
